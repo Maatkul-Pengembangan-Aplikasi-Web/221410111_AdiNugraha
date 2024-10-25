2b1a3dc9cf108f5acd80cd7e94129bce75772c6c
Nugrahoa <nugraharoka@gmail.com>
2024-10-25 13:34:01 +0700

    +halaman program studi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('/prodi')" :active="request()->routeIs('/prodi')">
+                        {{ __('Program Studi') }}
+                    </x-nav-link>
                 </div>
             </div>
 
---
+++ resources/views/prodi/index.blade.php
@@ -0,0 +1,48 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Program Studi') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <div class="ml-auto d-flex">
+                            <a href="#" class="btn btn-primary mr-2">Tambah Program Studi</a>
+                            <form action="" method="GET" class="d-flex">
+                                <input type="text" name="search" class="form-control" placeholder="Pencarian">
+                                <button class="btn btn-primary ml-2" type="submit">
+                                    <i class="bi bi-search"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <table class="table table-hover">
+                        <thead class="table-primary">
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Program Studi</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="#"class="btn btn-secondary">Edit</a>
+                                    <a href="#"class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
